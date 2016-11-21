021d8183444f7a1e7b2020d1ee474289652c51bf
renyp <beijingryp@163.com>
2016-11-21 10:55:40 +0800

    修改推送测试

--- src/com/renyp/testGit/hello/HelloWorld.java
+++ src/com/renyp/testGit/hello/HelloWorld.java
@@ -1,10 +1,14 @@
 package com.renyp.testGit.hello;
 
+import java.util.Calendar;
+
 /**
  * Created by renyp on 2016/11/21.
  */
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.print("Hello World!");
+
+        System.out.print("Hello World For Idea!");
+      //  System.out.print(Calendar.getInstance().getTime());
     }
 }
