5959f9bae1a15ca01ada2a9e3d8f54fea028df7b
renyp <beijingryp@163.com>
2016-11-21 10:43:30 +0800

    修改项目名

---
+++ src/com/renyp/testGit/hello/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.renyp.testGit.hello;
+
+/**
+ * Created by renyp on 2016/11/21.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.print("Hello World!");
+    }
+}
